4f9ebb8f5981feefab6358a90dd0cd97ec1be39f
sergio99778 <sergio4brandon@gmail.com>
2021-03-07 13:10:21 -0600

    Adding 0 to values < 10.

--- js/app.dev.js
+++ js/app.dev.js
@@ -6,6 +6,16 @@ const getHour = () => {
         minute: date.getMinutes(),
         seconds: date.getSeconds()
     }
+    if(time.hour<10){
+        time.hour = "0" +time.hour;
+    }
+    if(time.minute<10){
+        time.minute = "0" +time.minute;
+    }
+    if(time.seconds<10){
+        time.seconds = "0" +time.seconds;
+    }
     clock.innerHTML = `${time.hour} : ${time.minute} : ${time.seconds}`;
 };
+
 setInterval(getHour,100)
